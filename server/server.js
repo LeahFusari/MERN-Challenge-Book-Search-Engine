70f3184c00935dacb3d1e52187fc1012579f9602
LeahFusari <87339742+LeahFusari@users.noreply.github.com>
2022-02-25 14:23:28 -0500

    added mutations and queries files as well as added Apollo Provider to App.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
-
-
 const db = require('./config/connection');
-
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -29,7 +25,6 @@ if (process.env.NODE_ENV === 'production') {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
 
 
 const startServer = async () => {
